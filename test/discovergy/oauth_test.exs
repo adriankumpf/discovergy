31f34e6fc8f8394df15b62478378675ee0f91fe2
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2021-10-27 15:53:22 +0200

    Update tests

--- test/discovergy/oauth_test.exs
+++ test/discovergy/oauth_test.exs
@@ -21,8 +21,8 @@ defmodule Discovergy.OAuthTest do
            } == token
   end
 
-  test "resuses the consumer" do
-    mock(&authorization_without_requesting_consumer/1)
+  test "does not resuse the consumer" do
+    mock(&full_authorization/1)
 
     consumer = %Discovergy.OAuth.Consumer{
       attributes: %{},
@@ -32,9 +32,11 @@ defmodule Discovergy.OAuthTest do
       secret: "$secret"
     }
 
-    assert {:ok, %Discovergy.Client{consumer: ^consumer}} =
+    assert {:ok, %Discovergy.Client{consumer: new_consumer}} =
              Discovergy.Client.new(adapter: Tesla.Mock, consumer: consumer)
              |> Discovergy.Client.login("$email", "$password")
+
+    assert new_consumer != consumer
   end
 
   defp full_authorization(%Tesla.Env{} = env) do
@@ -42,27 +44,6 @@ defmodule Discovergy.OAuthTest do
       %Tesla.Env{method: :post, url: "https://api.discovergy.com/public/v1/oauth1/consumer_token"} ->
         json(%{key: "$key", secret: "$secret", owner: "$client_id", attributes: %{}})
 
-      %Tesla.Env{method: :post, url: "https://api.discovergy.com/public/v1/oauth1/request_token"} ->
-        text(
-          "oauth_token=$oauth_token&oauth_token_secret=$oauth_token_secret&oauth_callback_confirmed=true",
-          headers: [{"content-type", "application/x-www-form-urlencoded"}]
-        )
-
-      %Tesla.Env{method: :get, url: "https://api.discovergy.com/public/v1/oauth1/authorize"} ->
-        text("oauth_verifier=$oauth_verifier",
-          headers: [{"content-type", "application/x-www-form-urlencoded"}]
-        )
-
-      %Tesla.Env{method: :post, url: "https://api.discovergy.com/public/v1/oauth1/access_token"} ->
-        text(
-          "oauth_token=$access_token&oauth_token_secret=$access_token_secret",
-          headers: [{"content-type", "application/x-www-form-urlencoded"}]
-        )
-    end
-  end
-
-  defp authorization_without_requesting_consumer(%Tesla.Env{} = env) do
-    case env do
       %Tesla.Env{method: :post, url: "https://api.discovergy.com/public/v1/oauth1/request_token"} ->
         text(
           "oauth_token=$oauth_token&oauth_token_secret=$oauth_token_secret&oauth_callback_confirmed=true",
