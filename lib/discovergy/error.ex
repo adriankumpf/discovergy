cbfe5d4583ce24d14c73720c1d83c2fb045c7957
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2023-08-27 22:07:58 +0200

    Fix Error.message/1

--- lib/discovergy/error.ex
+++ lib/discovergy/error.ex
@@ -6,14 +6,20 @@ defmodule Discovergy.Error do
   alias Discovergy.HTTPClient
 
   @type t :: %__MODULE__{
-          reason: atom() | String.t(),
+          reason: term(),
           response: {HTTPClient.status(), HTTPClient.headers(), HTTPClient.body()}
         }
 
   defexception [:reason, :response]
 
   @impl true
+  def message(%__MODULE__{reason: reason}) when is_binary(reason), do: reason
+
+  def message(%__MODULE__{reason: reason}) when is_exception(reason) do
+    Exception.message(reason)
+  end
+
   def message(%__MODULE__{reason: reason}) do
-    to_string(reason)
+    inspect(reason)
   end
 end
